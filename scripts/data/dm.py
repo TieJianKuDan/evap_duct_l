9c2c47956bf7b1fe2d9a9f53ead1ffc24d4f54c1
tiejiankudan <893507220@qq.com>
2023-12-29 14:47:52 +0800

    add duct height data module

---
+++ scripts/data/dm.py
@@ -0,0 +1,206 @@
+import gc
+import os
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import xarray as xr
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset, random_split
+from torchvision import transforms
+
+root_dir = os.getcwd()
+
+class CelebADataset(Dataset):
+
+    def __init__(self, root, img_shape=(32, 32)) -> None:
+        super().__init__()
+        self.root = root
+        self.img_shape = img_shape
+        self.filenames = sorted(os.listdir(root))
+
+    def __len__(self) -> int:
+        return len(self.filenames)
+
+    def __getitem__(self, index: int):
+        path = os.path.join(self.root, self.filenames[index])
+        img = Image.open(path).convert('RGB')
+        pipeline = transforms.Compose([
+            transforms.CenterCrop(168),
+            transforms.Resize(self.img_shape),
+            transforms.ToTensor()
+        ])
+        return pipeline(img)
+
+
+class CelebaPLDM(pl.LightningDataModule):
+
+    def __init__(self, data_config, batch_size, seed):
+        super(CelebaPLDM, self).__init__()
+        self.data_dir = root_dir + "\data\img_align_celeba"
+        self.val_ratio = data_config.val_ratio
+        self.test_ratio = data_config.test_ratio
+        self.num_workers = data_config.num_workers
+        self.persistent_workers = data_config.persistent_workers
+        self.batch_size = batch_size
+        self.seed = seed
+
+    @property
+    def train_sample_num(self):
+        return len(self.train_set)
+    
+    @property
+    def val_sample_num(self):
+        return len(self.val_set)
+    
+    @property
+    def test_sample_num(self):
+        return len(self.test_set)
+
+    def setup(self, stage=None):
+        datasets = CelebADataset(self.data_dir)
+        self.train_set, self.val_set, self.test_set = \
+            random_split(
+                datasets, 
+                [
+                    1 - self.val_ratio - self.test_ratio, 
+                    self.val_ratio, 
+                    self.test_ratio
+                ], 
+                generator=torch.Generator().manual_seed(self.seed)
+            )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_set, 
+            batch_size=self.batch_size, 
+            shuffle=True,
+            num_workers=self.num_workers,
+            persistent_workers=self.persistent_workers
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_set, 
+            batch_size=self.batch_size, 
+            shuffle=False,
+            num_workers=self.num_workers,
+            persistent_workers=self.persistent_workers
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_set, 
+            batch_size=self.batch_size, 
+            shuffle=False,
+            num_workers=self.num_workers,
+            persistent_workers=self.persistent_workers
+        )
+    
+class EDHDataset(Dataset):
+
+    def __init__(self, root) -> None:
+        super().__init__()
+        self._edh = []
+        for root, _, files in os.walk(root):  
+            for filename in files:
+                self._edh.append(
+                    xr.open_dataset(os.path.join(root, filename))
+                )
+        self._edh = sorted(
+            self._edh, key=lambda _edh: _edh.time.data[0]
+        )
+        self.lon = self._edh[0].longitude.data
+        self.lat = self._edh[0].latitude.data
+        self.edh = None
+        self.time = None
+        for i in range(len(self._edh)):
+            if self.edh is None:
+                self.edh = self._edh[i].EDH.data
+            else:
+                self.edh = np.concatenate(
+                    (self.edh, self._edh[i].EDH.data),
+                    axis=0
+                )
+            if self.time is None:
+                self.time = self._edh[i].time.data
+            else:
+                self.time = np.concatenate(
+                    (self.time, self._edh[i].time.data),
+                    axis=0
+                )
+        del(self._edh)
+        gc.collect()
+
+    def __len__(self) -> int:
+        return len(self.time)
+
+    def __getitem__(self, index: int):
+        return self.edh[index]
+    
+class EDHPLDM(pl.LightningDataModule):
+
+    def __init__(self, data_config, batch_size, seed):
+        super(EDHPLDM, self).__init__()
+        self.data_dir = root_dir + "\data\edh"
+        self.val_ratio = data_config.val_ratio
+        self.test_ratio = data_config.test_ratio
+        self.num_workers = data_config.num_workers
+        self.persistent_workers = data_config.persistent_workers
+        self.batch_size = batch_size
+        self.seed = seed
+
+    @property
+    def train_sample_num(self):
+        return len(self.train_set)
+    
+    @property
+    def val_sample_num(self):
+        return len(self.val_set)
+    
+    @property
+    def test_sample_num(self):
+        return len(self.test_set)
+
+    def setup(self, stage=None):
+        datasets = EDHDataset(self.data_dir)
+        self.lon = datasets.lon
+        self.lat = datasets.lat
+        self.time = datasets.time
+        self.train_set, self.val_set, self.test_set = \
+            random_split(
+                datasets, 
+                [
+                    1 - self.val_ratio - self.test_ratio, 
+                    self.val_ratio, 
+                    self.test_ratio
+                ], 
+                generator=torch.Generator().manual_seed(self.seed)
+            )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_set, 
+            batch_size=self.batch_size, 
+            shuffle=True,
+            num_workers=self.num_workers,
+            persistent_workers=self.persistent_workers
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_set, 
+            batch_size=self.batch_size, 
+            shuffle=False,
+            num_workers=self.num_workers,
+            persistent_workers=self.persistent_workers
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_set, 
+            batch_size=self.batch_size, 
+            shuffle=False,
+            num_workers=self.num_workers,
+            persistent_workers=self.persistent_workers
+        )
---
+++ scripts/data/view.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from pandas import to_datetime
+
+
+def dt64todt(dt64):
+    ts = to_datetime(dt64).timestamp()
+    return datetime.fromtimestamp(ts)
+
+def edh_plot(lon, lat, data):
+    fig = plt.figure(figsize=(5, 5))
+    proj = ccrs.PlateCarree(central_longitude=180)
+    axe:Axes = plt.axes(projection=proj)
+    axe.gridlines(
+        draw_labels=True, dms=True, 
+        x_inline=False, y_inline=False
+    )
+    axe.coastlines()
+    axe.add_feature(cfeature.OCEAN)
+    axe.add_feature(cfeature.LAND, edgecolor='b')
+
+    levels = [0,10,20,30,40,50,60,70,80,90]
+    contours = axe.contourf(
+        lon, lat, data, 
+        levels=levels,
+        extend="both",
+        transform=proj
+    )
+    fig.colorbar(contours, shrink=0.6, pad=0.15)
+    return fig
+
+def edh_subplot(lon, lat, data, row, col):
+    fig = plt.figure(
+        figsize=(5*col, 5*row)
+    )
+    proj = ccrs.PlateCarree(central_longitude=180)
+    axes = fig.subplots(
+        row, col, 
+        subplot_kw={'projection':proj}
+    ).reshape((-1,))
+    for i in range(len(axes)):
+        axes[i].gridlines(
+            draw_labels=True, dms=True, 
+            x_inline=False, y_inline=False
+        )
+        axes[i].coastlines()
+        axes[i].add_feature(cfeature.OCEAN)
+        axes[i].add_feature(cfeature.LAND, edgecolor='b')
+
+        levels = [0,10,20,30,40,50,60,70,80,90]
+        contours = axes[i].contourf(
+            lon, lat, data[i], 
+            levels=levels,
+            extend="both",
+            transform=ccrs.PlateCarree()
+        )
+        fig.colorbar(
+            contours, shrink=0.6,
+            ax=axes[i], pad=0.15
+        )
+    fig.tight_layout()
+    return fig
+
+def edh_animate(lon, lat, data, time=None):
+    plt.ioff()
+    plt.figure(figsize=(5, 5))
+    proj = ccrs.PlateCarree(central_longitude=0)
+    levels = [0,10,20,30,40,50,60,70,80,90]
+    for i in range(data.shape[0]):
+        axe:Axes = plt.axes(projection=proj)
+        # axe.set_extent([-180, 180, -90, 90])
+        axe.gridlines(
+            draw_labels=True, dms=True, 
+            x_inline=False, y_inline=False
+        )
+        if time is not None:
+            axe.set_title(
+                dt64todt(time[i]).strftime(r"%Y-%m-%d %H")
+            )
+        axe.coastlines()
+        axe.add_feature(cfeature.OCEAN)
+        axe.add_feature(cfeature.LAND, edgecolor='b')
+        axe.contourf(
+            lon, lat, data[i], 
+            levels=levels,
+            extend="both",
+            transform=proj
+        )
+        plt.draw()
+        plt.pause(0.05)
+        plt.clf()
